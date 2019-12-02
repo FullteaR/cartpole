56648130ad34571ea969ad8c24f03c8c9f9d85e4
Rikuta Furuta <rikuta@furutan.com>
2019-12-02 17:24:46 +0900

    fix: sample.py
    手で入力して遊ぶゲームにする

--- sample.py
+++ sample.py
@@ -1,7 +1,11 @@
 import gym
 env = gym.make('CartPole-v0')
-env.reset()
-for _ in range(1000):
-    env.render()
-    env.step(env.action_space.sample()) # take a random action
+for i_episode in range(20):
+    observation = env.reset()
+    for t in range(1000):
+        env.render()
+        print(observation)
+        action=int(input())# must be 0(left) or 1(right)
+        observation, reward, done, info = env.step(action)
+
 env.close()
