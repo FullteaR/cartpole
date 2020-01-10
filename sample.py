0b1005743fc9f27b3abc998b2715ba8c3a61d9b5
Rikuta Furuta <rikuta@furutan.com>
2020-01-11 03:43:04 +0900

    add: anim.ipynb
    アニメ化したい

--- sample.py
+++ sample.py
@@ -1,11 +1,32 @@
 import gym
+import numpy as np
+import random
 env = gym.make('CartPole-v0')
-for i_episode in range(20):
-    observation = env.reset()
-    for t in range(1000):
-        env.render()
-        print(observation)
-        action=int(input())# must be 0(left) or 1(right)
-        observation, reward, done, info = env.step(action)
+
+
+
+q_table = {}
+
+def get_action(next_state, episode):
+           #徐々に最適行動のみをとる、ε-greedy法
+    epsilon = 0.5 * (1 / (episode + 1))
+    if epsilon <= np.random.uniform(0, 1):
+        next_action = np.argmax(q_table.get(next_state,(random.random(),random.random()))
+    else:
+        next_action = np.random.choice([0, 1])
+    return next_action
+
+def update_Qtable(q_table, state, action, reward, next_state):
+    gamma = 0.99
+    alpha = 0.5
+    next_Max_Q=max(q_table[next_state][0],q_table[next_state][1] )
+    q_table[state, action] = (1 - alpha) * q_table[state, action] +\
+            alpha * (reward + gamma * next_Max_Q)
+
+    return q_table
+
+
+def train(render=False):
+
 
 env.close()
